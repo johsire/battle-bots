c84ae369b5cdf927998863a60364c0b2c14ec99e
Josiah Maina <josiah5115@gmail.com>
2018-10-09 15:38:09 -0600

    build my components

---
+++ src/components/Collection.vue
@@ -0,0 +1,6 @@
+
+<template>
+ <div class="collection">
+  <h1>This is the Collection Component</h1>
+ </div>
+</template>
---
+++ src/components/Create.vue
@@ -0,0 +1,24 @@
+
+<template>
+ <div class="create">
+  <h1>Create a New Bot to Start</h1>
+
+  <p>Bot's Name*     <input type="text" v-model="name"></p>
+  <p>Attack Value* <input type="number" v-model="attack"></p>
+  <p>Health Value* <input type="number" v-model="health"></p>
+
+  <button></button>
+  <button></button>
+
+ </div>
+</template>
+
+<script>
+
+ export default {
+   data: function() {
+
+   }
+ }
+
+</script>
